9b198d292a470709543af344ae0ddad8f639fae3
zhangzibin <2319867935@qq.com>
2017-06-12 18:42:50 +0800

    抢红包

---
+++ app/src/main/java/com/zzb/redenvelope/job/BaseAccessbilityJob.java
@@ -0,0 +1,36 @@
+package com.zzb.redenvelope.job;
+
+import android.content.Context;
+
+import com.zzb.redenvelope.Config;
+import com.zzb.redenvelope.QiangHongBaoService;
+
+
+/**
+ * <p>Created 16/1/16 上午12:38.</p>
+ * <p><a href="mailto:codeboy2013@gmail.com">Email:codeboy2013@gmail.com</a></p>
+ * <p><a href="http://www.happycodeboy.com">LeonLee Blog</a></p>
+ *
+ * @author LeonLee
+ */
+public abstract class BaseAccessbilityJob implements AccessbilityJob {
+
+    private QiangHongBaoService service;
+
+    @Override
+    public void onCreateJob(QiangHongBaoService service) {
+        this.service = service;
+    }
+
+    public Context getContext() {
+        return service.getApplicationContext();
+    }
+
+    public Config getConfig() {
+        return service.getConfig();
+    }
+
+    public QiangHongBaoService getService() {
+        return service;
+    }
+}
